c00e9b774b8ea6f00018111b51b5f1be9284e5a1
Maksim <email@mail.ru>
2023-07-01 11:51:37 +0400

    fix удаление лишних import

--- view/InfoWindow.xaml.cs
+++ view/InfoWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace accounting_platform.view
 {
@@ -22,6 +12,11 @@ namespace accounting_platform.view
         public InfoWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_Initialized(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         }
     }
 }
