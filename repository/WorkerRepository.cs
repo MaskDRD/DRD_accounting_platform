16647aacce00616f4a747c5550c7de18ad77ac5b
Maksim <email@mail.ru>
2023-07-02 09:59:32 +0400

    AB-4 работа с бд

---
+++ repository/WorkerRepository.cs
@@ -0,0 +1,14 @@
+﻿using accounting_platform.utl;
+using System.Data;
+
+namespace accounting_platform.repository
+{
+    class WorkerRepository
+    {
+        public DataTable selectWorker()
+        {
+            string sql = "call worker_get()";
+            return ConnentMain.Instance.getDataTableSql(sql);
+        }
+    }
+}
